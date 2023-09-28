a4b4bdaf9be17db3cca703f2af43945006a4a9d0
Mrgiddy001 <143078458+Mrgiddy001@users.noreply.github.com>
2023-09-28 11:09:41 +0300

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String [] args){
+        /*int[] Numbers = {8,7,6,5,4};
+        System.out.println(Numbers[0]);*/
+        String [][] colours= new String[2][2];
+        colours[0][0] = "Yellow";
+        colours[0][1]="Green";
+        colours[1][0] = "Blue";
+        colours[1][1] = "Pink";
+        System.out.println(colours[1][1]);
+    }
+}
