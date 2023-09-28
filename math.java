c5350c538ed377f2cf9346fee6744576218e4f39
Mrgiddy001 <143078458+Mrgiddy001@users.noreply.github.com>
2023-09-28 11:11:07 +0300

    Create math.java

---
+++ math.java
@@ -0,0 +1,17 @@
+public class math {
+    public static void main(String[] args){
+        double x,a,b,c,d,e,r;
+        x=4.2;a=x;b=x; c=x;d=x;e=x;r=x;
+        double g,f,y= 4.9;f=y;g=y;
+        x = Math.abs(x);
+        double z = Math.max(e,y);
+        double q= Math.min(f,r);
+        a=Math.ceil(a);
+        b=Math.floor(b);
+        c=Math.sqrt(c);
+        g=Math.sqrt(g);
+        System.out.println(x+"\n"+z+"\n" +q+"\n"+a+"\n"+b+"\n"+c+"\n"+g);
+
+    }
+
+}
