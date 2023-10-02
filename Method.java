8d86b637ea47999edc9ac0ea4022f8fc4b1b7ee2
Mr Giddy <143078458+Mrgiddy001@users.noreply.github.com>
2023-10-02 12:52:51 +0300

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,9 @@
+public class Method {
+    public static void main (String[] args){
+        String name = "Alex";
+        int Age = 90;
+        hello(name,Age);
+    }
+    static void hello(String name,int Age){
+    System.out.println("My name is "+name+" and I am "+Age+" years old.☺☻");}
+}
